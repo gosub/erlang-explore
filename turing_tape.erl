99e601ee891c76e13689d8592417b11bfd747ced
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2021-10-11 19:30:28 +0200

    turing_tape: module for simulating a turing machine infite tape

---
+++ turing_tape.erl
@@ -0,0 +1,50 @@
+-module(turing_tape).
+
+-export([new_tape/0, left/1, right/1, write/2, clear/1, read/1, eval/2, eval_list/2]).
+
+new_tape() ->
+    {{}, [], []}.
+
+left({{}, [], []}) ->
+    {{}, [], []};
+left({Cursor, [], Right}) ->
+    {{}, [], [Cursor|Right]};
+left({{}, [X|Rest], []}) ->
+    {X, Rest, []};
+left({Cursor, [X|Rest], Right}) ->
+    {X, Rest, [Cursor|Right]}.
+
+right({{}, [], []}) ->
+    {{}, [], []};
+right({Cursor, Left, []}) ->
+    {{}, [Cursor|Left], []};
+right({{}, [], [X|Rest]}) ->
+    {X, [], Rest};
+right({Cursor, Left, [X|Rest]}) ->
+    {X, [Cursor|Left], Rest}.
+
+write({_, L, R}, X) ->
+    {{X}, L, R}.
+
+clear({_, L, R}) ->
+    {{}, L, R}.
+
+read({{}, _, _}) ->
+    empty;
+read({{X}, _, _}) ->
+    {ok, X}.
+
+eval(Tape, left) ->
+    left(Tape);
+eval(Tape, right) ->
+    right(Tape);
+eval(Tape, clear) ->
+    clear(Tape);
+eval(Tape, {write, X}) ->
+    write(Tape, X).
+
+eval_list(Tape, []) ->
+    Tape;
+eval_list(Tape, [Cmd|Rest]) ->
+    T2 = eval(Tape, Cmd),
+    eval_list(T2, Rest).
