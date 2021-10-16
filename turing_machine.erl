afdd36a77a57f72272039abdfbab5722db5a892c
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2021-10-16 14:32:03 +0200

    turing_machine: turn the crank once with step/1

--- turing_machine.erl
+++ turing_machine.erl
@@ -1,10 +1,16 @@
 -module(turing_machine).
 -author("Giampaolo Guiducci <giampaolo.guiducci@gmail.com>").
 
--export([new/2, new/3]).
+-export([new/2, new/3, step/1]).
 
 new(Table, InitState) ->
     new(Table, InitState, turing_tape:new()).
 
 new(Table, InitState, Tape) ->
     {Table, InitState, Tape}.
+
+step({Table, State, Tape}) ->
+    Read = turing_tape:read(Tape),
+    {ok, {Actions, NewState}} = maps:find({State, Read}, Table),
+    NewTape = turing_tape:eval_list(Tape, Actions),
+    {Table, NewState, NewTape}.
