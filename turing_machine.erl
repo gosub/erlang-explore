51e945b1cc89bcf7656106c552c7bdc77e3e4d98
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2021-10-16 13:35:39 +0200

    turing_machine: create a new one with new/2 and new/3

--- turing_machine.erl
+++ turing_machine.erl
@@ -1,5 +1,10 @@
 -module(turing_machine).
 -author("Giampaolo Guiducci <giampaolo.guiducci@gmail.com>").
 
--export([]).
+-export([new/2, new/3]).
 
+new(Table, InitState) ->
+    new(Table, InitState, turing_tape:new()).
+
+new(Table, InitState, Tape) ->
+    {Table, InitState, Tape}.
