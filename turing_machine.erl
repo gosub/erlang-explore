f10280b3b8fca987fce4d1982fa4f1be9efa8ee9
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2021-10-16 14:56:43 +0200

    turing_machine: show state and tape content with to_string/1

--- turing_machine.erl
+++ turing_machine.erl
@@ -1,7 +1,7 @@
 -module(turing_machine).
 -author("Giampaolo Guiducci <giampaolo.guiducci@gmail.com>").
 
--export([new/2, new/3, step/1]).
+-export([new/2, new/3, step/1, to_string/1]).
 
 new(Table, InitState) ->
     new(Table, InitState, turing_tape:new()).
@@ -14,3 +14,8 @@ step({Table, State, Tape}) ->
     {ok, {Actions, NewState}} = maps:find({State, Read}, Table),
     NewTape = turing_tape:eval_list(Tape, Actions),
     {Table, NewState, NewTape}.
+
+to_string({_, State, Tape}) ->
+    StateStr = lists:concat([State]),
+    TapeStr = turing_tape:to_string(Tape),
+    "state: " ++ StateStr ++ ", tape: " ++ TapeStr.
