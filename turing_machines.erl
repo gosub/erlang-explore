3f1afd9a041a5f8de177b5bed1e5ec80be96c8f7
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2021-10-20 21:27:15 +0200

    turing_machines: a simplified version of the alternating 0/1 output

--- turing_machines.erl
+++ turing_machines.erl
@@ -2,7 +2,7 @@
 -author("Giampaolo Guiducci <giampaolo.guiducci@gmail.com>").
 
 -export([make/1, make/2]).
--export([alternating_example/0]).
+-export([alternating_example/0, simplified_alternating_example/0]).
 
 make(Machine) ->
     make(Machine, "𝔟").
@@ -17,4 +17,11 @@ alternating_example() ->
  "𝔠", {}, [right],             "𝔢",
  "𝔢", {}, [{write, 1}, right], "𝔨",
  "𝔨", {}, [right],             "𝔟"
+].
+
+simplified_alternating_example() ->
+[
+ "𝔟", {}, [{write, 0}],               "𝔟",
+ "𝔟", 0,  [right, right, {write, 1}], "𝔟",
+ "𝔟", 1,  [right, right, {write, 0}], "𝔟"
 ].
