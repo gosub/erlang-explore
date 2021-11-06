71d729b6ab2320a73b7f78c7833aebd18fcaa823
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2021-11-06 20:06:27 +0100

    tmsh: renamed eval/2 function (no need to repeat module name)

--- src/tmsh.erl
+++ src/tmsh.erl
@@ -5,15 +5,16 @@
 -export([run/1]).
 
 run(TM) ->
-    mini_repl:run("tmsh> ", fun tmsh_eval/2, TM).
+    mini_repl:run("tmsh> ", fun eval/2, TM).
 
-tmsh_eval(_, Input) when Input=:="q" orelse Input=:="quit" ->
+eval(_, Input) when Input=:="q" orelse Input=:="quit" ->
     quit;
-tmsh_eval(TM, Input) when Input=:="p" ->
+eval(TM, Input) when Input=:="p" ->
     {ok, turing_machine:to_string(TM), TM};
-tmsh_eval(TM, Input) when Input=:="n" ->
+eval(TM, Input) when Input=:="n" ->
     TM2 = turing_machine:step(TM),
     {ok, turing_machine:to_string(TM2), TM2};
-tmsh_eval(TM, _) ->
+eval(TM, _) ->
     {ok, "Command not found.", TM}.
+
 
