08dfda1613600fb1033440b8d595e844f433fb1f
Giampaolo Guiducci <giampaolo.guiducci@gmail.com>
2021-11-06 20:39:15 +0100

    tmsh: refactored some functions out of eval/2

--- src/tmsh.erl
+++ src/tmsh.erl
@@ -4,17 +4,25 @@
 
 -export([run/1]).
 
+
 run(TM) ->
     mini_repl:run("tmsh> ", fun eval/2, TM).
+
 
 eval(_, Input) when Input=:="q" orelse Input=:="quit" ->
     quit;
 eval(TM, Input) when Input=:="p" ->
-    {ok, turing_machine:to_string(TM), TM};
+    print_machine(TM);
 eval(TM, Input) when Input=:="n" ->
-    TM2 = turing_machine:step(TM),
-    {ok, turing_machine:to_string(TM2), TM2};
+    step_machine(TM);
 eval(TM, _) ->
     {ok, "Command not found.", TM}.
 
 
+print_machine(TM) ->
+    {ok, turing_machine:to_string(TM), TM}.
+
+
+step_machine(TM) ->
+    TM2 = turing_machine:step(TM),
+    {ok, turing_machine:to_string(TM2), TM2}.
